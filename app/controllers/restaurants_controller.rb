aadf302789f947e0e592e572cb22421fffd4ab75
Shunichi Ikegami <sike.tm@gmail.com>
2015-03-11 15:39:44 +0900

    ログインしてないと編集や登録ができないように

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,6 @@
 class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: %i(index show)
 
   def index
     @restaurants = Restaurant.all

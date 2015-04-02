fac7132c52d930f39f9989b09dd5238dc7ad4284
Shunichi Ikegami <sike.tm@gmail.com>
2015-04-02 11:15:53 +0900

    production での smtp 設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,17 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :enable_starttls_auto => true,
+      :address => 'smtp.mandrillapp.com',
+      :port => '587',
+      :domain => 'heroku.com',
+      :authentication => 'plain',
+      :user_name => ENV['MANDRILL_USERNAME'],
+      :password => ENV['MANDRILL_APIKEY']
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).

3b65c293ad454d0e21df1dff89fc02c066c77f4b
Shunichi Ikegami <sike.tm@gmail.com>
2015-04-02 21:22:31 +0900

    メールアドレスの確認処理を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :reviews
 
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :name, presence: true, uniqueness: true, length: { maximum: 32 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150311092426) do
+ActiveRecord::Schema.define(version: 20150402024949) do
 
   create_table "restaurants", force: true do |t|
     t.string   "name"
@@ -65,8 +65,12 @@ ActiveRecord::Schema.define(version: 20150311092426) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name",                   default: "", null: false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["name"], name: "index_users_on_name", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true

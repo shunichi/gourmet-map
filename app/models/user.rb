90ae863a1b676c411f6aa306c3f84743c608c802
Shunichi Ikegami <sike.tm@gmail.com>
2015-04-03 09:37:40 +0900

    サインアップ可能なメールアドレスを指定可能に

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,21 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :name, presence: true, uniqueness: true, length: { maximum: 32 }
+  validates :name, presence: true, uniqueness: true, length: { maximum: 16 }
+  validate :allowed_email
+
+  def allowed_email
+    email = self.email.downcase
+    if ENV['ADMIN_EMAILS']
+      admin_emails = ENV['ADMIN_EMAILS'].split(',').map(&:strip).map(&:downcase)
+      return if admin_emails.member?(email)
+    end
+    if ENV['EMAIL_REGEXPS']
+      email_regexps = ENV['EMAIL_REGEXPS'].split(',').map{ |pat| Regexp.new(pat.strip) }
+      unless email_regexps.any?{ |r| r.match(email) }
+        errors.add(:email, 'そのメールアドレスでのユーザー登録は受け付けていません')
+      end
+    end
+  end
+
 end

ca55e0992a25af20e0ba5b53f8e48364fb4fd6ed
Shunichi Ikegami <sike.tm@gmail.com>
2015-04-09 10:10:03 +0900

    レビューが投稿されたら店情報の更新時間も更新

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :user
-  belongs_to :restaurant
+  belongs_to :restaurant, touch: true
 
   validates :user_id, presence: true
   validates :restaurant_id, presence: true

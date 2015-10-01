017af822f1a1c42538ddf8d7603eb45563704b8f
Shunichi Ikegami <sike.tm@gmail.com>
2015-10-01 10:29:02 +0900

    heroku deploy error チェック用コード追加

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,16 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-      provider:              'AWS',
-      aws_access_key_id:     ENV['AWS_ACCESS_KEY'],
-      aws_secret_access_key: ENV['AWS_SECRET'],
-      region:                ENV['AWS_REGION'],
-  }
-  config.fog_directory  = ENV['AWS_S3_BUCKET']
-  config.fog_public     = true
+if ENV['AWS_ACCESS_KEY'].present?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+        provider:              'AWS',
+        aws_access_key_id:     ENV['AWS_ACCESS_KEY'],
+        aws_secret_access_key: ENV['AWS_SECRET'],
+        region:                ENV['AWS_REGION'],
+    }
+    config.fog_directory  = ENV['AWS_S3_BUCKET']
+    config.fog_public     = true
 
-  config.storage = Rails.env.development? ? :file : :fog
+    config.storage = Rails.env.development? ? :file : :fog
+  end
 end
 
 CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
